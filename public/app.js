d57041d26cd879a77b539916439b62fc33785f16
Muhammad aaraiz <muhammadaaraiz619@gmail.com>
2023-11-26 16:42:24 +0500

    Work Completed

---
+++ public/app.js
@@ -0,0 +1,219 @@
+function revo(){
+    Swal.fire({
+        title: "Do you want to Continue To The Next Page?",
+        showDenyButton: true,
+        confirmButtonText: "Yes",
+        denyButtonText: `No`,
+        color: "white",
+        background: "black",
+      }).then((result) => {
+        if (result.isConfirmed) {
+            Swal.fire({
+                width: 400,
+                icon:"success",
+                title: "Successfully Login To The Main Page",
+               confirmButtonText: "OK",
+               color: "blue",
+               background: "black",
+            })
+            setTimeout(function () {
+                window.location.href = "revo.html";
+              }, 2000);
+        } else if (result.isDenied) {
+            Swal.fire({
+                width: 400,
+                icon:"error",
+                title: "Not Login To The Main Page",
+               showconfirmButton: true,
+               confirmButtonText: "OK",
+               color: "blue",
+               background: "black",
+            })
+        }
+      });
+}
+
+
+
+function prado(){
+  Swal.fire({
+      title: "Do you want to Continue To The Next Page?",
+      showDenyButton: true,
+      confirmButtonText: "Yes",
+      denyButtonText: `No`,
+      color: "white",
+      background: "black",
+    }).then((result) => {
+      if (result.isConfirmed) {
+          Swal.fire({
+              width: 400,
+              icon:"success",
+              title: "Successfully Login To The Main Page",
+             confirmButtonText: "OK",
+             color: "blue",
+             background: "black",
+          })
+          setTimeout(function () {
+              window.location.href = "prado.html";
+            }, 2000);
+      } else if (result.isDenied) {
+          Swal.fire({
+              width: 400,
+              icon:"error",
+              title: "Not Login To The Main Page",
+             showconfirmButton: true,
+             confirmButtonText: "OK",
+             color: "blue",
+             background: "black",
+          })
+      }
+    });
+}
+
+
+
+function fortuner(){
+  Swal.fire({
+      title: "Do you want to Continue To The Next Page?",
+      showDenyButton: true,
+      confirmButtonText: "Yes",
+      denyButtonText: `No`,
+      color: "white",
+      background: "black",
+    }).then((result) => {
+      if (result.isConfirmed) {
+          Swal.fire({
+              width: 400,
+              icon:"success",
+              title: "Successfully Login To The Main Page",
+             confirmButtonText: "OK",
+             color: "blue",
+             background: "black",
+          })
+          setTimeout(function () {
+              window.location.href = "fortuner.html";
+            }, 2000);
+      } else if (result.isDenied) {
+          Swal.fire({
+              width: 400,
+              icon:"error",
+              title: "Not Login To The Main Page",
+             showconfirmButton: true,
+             confirmButtonText: "OK",
+             color: "blue",
+             background: "black",
+          })
+      }
+    });
+}
+
+
+
+function mira(){
+  Swal.fire({
+      title: "Do you want to Continue To The Next Page?",
+      showDenyButton: true,
+      confirmButtonText: "Yes",
+      denyButtonText: `No`,
+      color: "white",
+      background: "black",
+    }).then((result) => {
+      if (result.isConfirmed) {
+          Swal.fire({
+              width: 400,
+              icon:"success",
+              title: "Successfully Login To The Main Page",
+             confirmButtonText: "OK",
+             color: "blue",
+             background: "black",
+          })
+          setTimeout(function () {
+              window.location.href = "Mira.html";
+            }, 2000);
+      } else if (result.isDenied) {
+          Swal.fire({
+              width: 400,
+              icon:"error",
+              title: "Not Login To The Main Page",
+             showconfirmButton: true,
+             confirmButtonText: "OK",
+             color: "blue",
+             background: "black",
+          })
+      }
+    });
+}
+
+
+
+function corolla(){
+  Swal.fire({
+      title: "Do you want to Continue To The Next Page?",
+      showDenyButton: true,
+      confirmButtonText: "Yes",
+      denyButtonText: `No`,
+      color: "white",
+      background: "black",
+    }).then((result) => {
+      if (result.isConfirmed) {
+          Swal.fire({
+              width: 400,
+              icon:"success",
+              title: "Successfully Login To The Main Page",
+             confirmButtonText: "OK",
+             color: "blue",
+             background: "black",
+          })
+          setTimeout(function () {
+              window.location.href = "Corolla.html";
+            }, 2000);
+      } else if (result.isDenied) {
+          Swal.fire({
+              width: 400,
+              icon:"error",
+              title: "Not Login To The Main Page",
+             showconfirmButton: true,
+             confirmButtonText: "OK",
+             color: "blue",
+             background: "black",
+          })
+      }
+    });
+}
+
+
+
+function cast(){
+  Swal.fire({
+      title: "Do you want to Continue To The Next Page?",
+      showDenyButton: true,
+      confirmButtonText: "Yes",
+      denyButtonText: `No`,
+      color: "white",
+      background: "black",
+    }).then((result) => {
+      if (result.isConfirmed) {
+          Swal.fire({
+              width: 400,
+              icon:"success",
+              title: "Successfully Login To The Main Page",
+             confirmButtonText: "OK",
+             color: "blue",
+             background: "black",
+          })
+          setTimeout(function () {
+              window.location.href = "Cast.html";
+            }, 2000);
+      } else if (result.isDenied) {
+          Swal.fire({
+              width: 400,
+              icon:"error",
+              title: "Not Login To The Main Page",
+             showconfirmButton: true,
+             confirmButtonText: "OK",
+             color: "blue",
+             background: "black",
+          })
+      }
+    });
+}
